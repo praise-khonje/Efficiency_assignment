231a11f81970a2e342c018d7e3c33df748b28d39
Magagatech <tithekhonje@gmail.com>
2025-03-25 16:04:19 +0200

    first commit

--- Effiencient code.py
+++ Effiencient code.py
@@ -1,9 +1,13 @@
 x = [3, 1, 5, 2, 4]
-target = 4
+target = 9
 
-# Linear search
-if target in x:
-    index = x.index(target)
-    print(f"Found at index {index}")
-else:
-    print("Not found")
+seen = set()
+printed = set()
+
+for num in x:
+    complement = target - num
+    pair = (min(num, complement), max(num, complement))
+    if complement in seen and pair not in printed:
+        print(f"Pair found: {pair[0]} + {pair[1]} = {target}")
+        printed.add(pair)
+    seen.add(num)
